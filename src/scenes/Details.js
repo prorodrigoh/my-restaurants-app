08ed455a02215f0376f2c101df3adbe19f581429
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-05-27 09:41:31 -0400

    corrected missing useState

--- src/scenes/Details.js
+++ src/scenes/Details.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { View, Text, Image, ActivityIndicator, Button } from 'react-native'
 import { SingleRestContext } from '../../App'
 import styles from '../styles'
